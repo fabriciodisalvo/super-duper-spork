d63f9a963a7fec15c2909415c2d1aafef76e902f
Fabricio Di Salvo <fabriciodisalvo@gmail.com>
2018-11-23 01:18:50 -0300

    Delete individual modules.

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
-from solver import sudoku_solver
-from display import sudoku_display
+from sudoku_class import Sudoku
+from solver_class import Solver
 
 
 sudoku_map_list = {
@@ -29,12 +29,31 @@ sudoku_map_list = {
                   8, 4, 5, 6, 9, 7, 3, 1, 2,
                   2, 3, 1, 5, 7, 4, 6, 9, 8,
                   9, 6, 8, 2, 3, 1, 5, 7, 4,
-                  5, 7, 4, 9, 6, 8, 2, 3, 1]
+                  5, 7, 4, 9, 6, 8, 2, 3, 1],
+    'half_miss': ['X', 2, 'X', 4, 'X', 6, 7, 'X', 9,
+                  7, 8, 'X', 1, 'X', 3, 'X', 5, 6,
+                  4, 5, 6, 7, 8, 9, 1, 2, 3,
+                  3, 1, 2, 'X', 4, 5, 'X', 6, 7,
+                  6, 9, 'X', 3, 1, 2, 8, 4, 5,
+                  8, 4, 5, 6, 9, 7, 3, 1, 2,
+                  2, 3, 1, 'X', 7, 'X', 'X', 9, 8,
+                  9, 6, 'X', 'X', 'X', 1, 'X', 7, 4,
+                  5, 7, 'X', 9, 6, 'X', 2, 3, 1],
+    'real_test': ['X', 'X', 'X', 'X', 'X', 4, 'X', 9, 'X',
+                  8, 'X', 2, 9, 7, 'X', 'X', 'X', 'X',
+                  9, 'X', 1, 2, 'X', 'X', 3, 'X', 'X',
+                  'X', 'X', 'X', 'X', 4, 9, 1, 5, 7,
+                  'X', 1, 3, 'X', 5, 'X', 9, 2, 'X',
+                  5, 7, 9, 1, 2, 'X', 'X', 'X', 'X',
+                  'X', 'X', 7, 'X', 'X', 2, 6, 'X', 3,
+                  'X', 'X', 'X', 'X', 3, 8, 2, 'X', 5,
+                  'X', 2, 'X', 5, 'X', 'X', 'X', 'X', 'X']
 }
 
-for sudoku in sudoku_map_list.keys():
+for i in sudoku_map_list.keys():
+    this_sudoku = Sudoku(sudoku_map_list[i])
     print()
     print(' Sudoku provided: ')
-    print(' Type: {}'.format(sudoku))
-    sudoku_display(sudoku_map_list[sudoku])
-    sudoku_solver(sudoku_map_list[sudoku])
+    this_sudoku.display()
+    sudoku_solved = Solver.solve(Solver, this_sudoku)
+
