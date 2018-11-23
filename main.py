b6d7ec1dc4456f417a887486fef28ebb2af90d04
Fabricio Di Salvo <fabriciodisalvo@gmail.com>
2018-11-23 01:18:24 -0300

    Update main.py
    Update main.py to accommodate to new classes: Sudoku and Solver.

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-from checker import sudoku_check
-from solver import sudoku_solver
-from display import sudoku_display
-
-
-def input_check(sudoku_map):
-    if len(sudoku_map) != 81:
-        print(' Sudoku provided is not 81 positions long...')
-    elif sudoku_map.count(0) != 0:
-        print(' Sudoku provided contains zeros...')
-    else:
-        return True
+from sudoku_class import Sudoku
+from solver_class import Solver
 
 
 def main():
@@ -22,16 +12,12 @@ def main():
                   2, 3, 1, 5, 7, 4, 6, 9, 8,
                   9, 6, 8, 2, 3, 1, 5, 7, 4,
                   5, 7, 4, 9, 6, 8, 'X', 3, 1]
-    if input_check(sudoku_map):
-        print('\n Sudoku provided: ')
-        sudoku_display(sudoku_map)
-        print()
-        if sudoku_check(sudoku_map):
-            print()
-            print(' Sudoku solved: ')
-            sudoku_display(sudoku_test_map)
-        else:
-            sudoku_solver(sudoku_map)
+    this_sudoku = Sudoku(sudoku_map)
+    print()
+    print(' Sudoku provided: ')
+    this_sudoku.display()
+    sudoku_solved = Solver.solve(Solver, this_sudoku)
+    return sudoku_solved
 
 
 if __name__ == "__main__":
