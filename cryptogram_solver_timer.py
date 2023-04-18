7f71531e8d49a1dba15982e063f806069dfd340d
fabriciodisalvo <fabriciodisalvo@gmail.com>
2023-04-18 20:49:16 +0200

    Create Timer file

---
+++ cryptogram_solver_timer.py
@@ -0,0 +1,17 @@
+import time
+
+timer_start = time.time() # Grab Currrent Time Before Running the Code
+
+from cryptogram_solver import * # This will run the whole module
+
+timer_end = time.time() # Grab Currrent Time After Running the Code
+timer_total_time = timer_end - timer_start # Subtract Start Time from The End Time
+
+f = open("log.txt", "a") # Update the log:
+log_text = str(version) + ' ' + str(timer_start) + ' ' + str(timer_total_time)
+f.write(log_text+"\n")
+f.close()
+
+f = open("log.txt", "r") # Open and read the updated log:
+print(f.read())
+f.close()
